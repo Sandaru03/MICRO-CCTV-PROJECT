74eba202733aca28f8fe88be9076567d9996cfb1
Sandaru03 <sandarudilshan24@gmail.com>
2025-08-13 09:44:01 +0530

    Create Initial Product ✏️

---
+++ models/product.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+
+    productId : {
+
+        type : String,
+        required : true,
+        unique : true
+    },
+
+    name : {
+
+        type : String,
+        required : true
+    },
+
+    altNames : {
+
+        type : [String],
+        default : []
+    },
+
+    labellPrice : {
+
+        type : Number,
+        required : true
+    },
+
+    price : {
+
+        type : Number,
+        required : true
+    },
+
+    images : {
+
+        type : [String],
+        default : ["/defult-product.jpg"]
+    },
+
+    description : {
+
+        type : String,
+        required : true
+    },
+
+    stock : {
+
+        type : Number,
+        required : true,
+        default : 0
+    },
+
+    isAvailable : {
+
+        type : Boolean,
+        required : true
+    },
+
+})
+
+const Product = mongoose.model("products",productSchema)
+
+export default Product
