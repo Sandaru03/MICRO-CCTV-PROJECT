a21d06e07bb72c5f522ac4b6ecbb7380ffa6283e
Sandaru03 <sandarudilshan24@gmail.com>
2025-08-21 23:15:06 +0530

    Product Update Page Create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,21 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './Pages/loginPage';
 import RegisterPage from './Pages/registerPage'; 
-import HomePage from './Pages/homePage';
 import { Toaster } from "react-hot-toast";
 import AdminPage from './Pages/adminPadge';
 import TestPage from './Pages/admin/testPage';
+import ClientWebPage from './Pages/customer/customerPage';
 
 function App() {
   return (
     <Router>
       <Toaster position="top-right"/> 
       <Routes>
-        <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<RegisterPage />} />
         <Route path="/admin/*" element={<AdminPage/>}/>
         <Route path="/test" element={<TestPage/>}/>
+        <Route path="/*" element={<ClientWebPage/>}/>
         
 
       </Routes>
---
+++ client/src/Pages/customer/customerPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../../assets/components/header";
+
+
+export default function ClientWebPage(){
+    return(
+
+         <div className="w-full h-screen max-h-screen">
+            <Header/>
+         </div>
+
+    )
+   
+}
